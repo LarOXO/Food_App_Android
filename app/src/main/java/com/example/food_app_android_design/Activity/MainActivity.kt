dd778a85c800f298a1257fc85901111f03c0ccb0
LarOXO <109023144+LarOXO@users.noreply.github.com>
2023-05-23 22:05:51 +0500

    design activity_intro.xml

--- app/src/main/java/com/example/food_app_android_design/Activity/MainActivity.kt
+++ app/src/main/java/com/example/food_app_android_design/Activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.food_app_android_design
+package com.example.food_app_android_design.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.food_app_android_design.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
